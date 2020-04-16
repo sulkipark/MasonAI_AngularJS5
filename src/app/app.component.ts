47929ffe87f97eb83827d3ea8bdff8401645a239
sulkipark <psk9314@gmail.com>
2020-04-16 13:58:59 +0900

    [U] angular 초기세팅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-quick-start';
+  // title = 'angular-quick-start';
+  title = '앵귤러퀵스타트';
 }
